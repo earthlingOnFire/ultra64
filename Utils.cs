34e3989755b42288f3125b35d69ea16abb2539d9
Headshotnoby <36455681+headshot2017@users.noreply.github.com>
2025-03-11 09:50:30 -0400

    surface vertices are int, not short

--- Utils.cs
+++ Utils.cs
@@ -15,15 +15,15 @@ namespace LibSM64
                     force = 0,
                     type = (short)surfaceType,
                     terrain = (ushort)terrainType,
-                    v0x = (short)(Interop.SCALE_FACTOR * -vertices[tris[i  ]].x),
-                    v0y = (short)(Interop.SCALE_FACTOR *  vertices[tris[i  ]].y),
-                    v0z = (short)(Interop.SCALE_FACTOR *  vertices[tris[i  ]].z),
-                    v1x = (short)(Interop.SCALE_FACTOR * -vertices[tris[i+2]].x),
-                    v1y = (short)(Interop.SCALE_FACTOR *  vertices[tris[i+2]].y),
-                    v1z = (short)(Interop.SCALE_FACTOR *  vertices[tris[i+2]].z),
-                    v2x = (short)(Interop.SCALE_FACTOR * -vertices[tris[i+1]].x),
-                    v2y = (short)(Interop.SCALE_FACTOR *  vertices[tris[i+1]].y),
-                    v2z = (short)(Interop.SCALE_FACTOR *  vertices[tris[i+1]].z)
+                    v0x = (int)(Interop.SCALE_FACTOR * -vertices[tris[i  ]].x),
+                    v0y = (int)(Interop.SCALE_FACTOR *  vertices[tris[i  ]].y),
+                    v0z = (int)(Interop.SCALE_FACTOR *  vertices[tris[i  ]].z),
+                    v1x = (int)(Interop.SCALE_FACTOR * -vertices[tris[i+2]].x),
+                    v1y = (int)(Interop.SCALE_FACTOR *  vertices[tris[i+2]].y),
+                    v1z = (int)(Interop.SCALE_FACTOR *  vertices[tris[i+2]].z),
+                    v2x = (int)(Interop.SCALE_FACTOR * -vertices[tris[i+1]].x),
+                    v2y = (int)(Interop.SCALE_FACTOR *  vertices[tris[i+1]].y),
+                    v2z = (int)(Interop.SCALE_FACTOR *  vertices[tris[i+1]].z)
                 });
             }
         }
